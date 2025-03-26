80712be7580c08d81686cc7011092c3630f3e69c
Ayush Tripathi <ayushflows@gmail.com>
2025-03-26 22:17:01 +0530

    added monaco editor

--- src/components/EditorSection.jsx
+++ src/components/EditorSection.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-sql";
-import "prismjs/themes/prism.css";
 import { Copy, Play, Save } from "lucide-react";
+import Editor from "@monaco-editor/react"; // Import Monaco Editor
 import styles from "../styles/SqlEditor.module.css";
 
 const EditorSection = ({ currentQuery, setCurrentQuery, setQueryResults }) => {
@@ -18,38 +15,36 @@ const EditorSection = ({ currentQuery, setCurrentQuery, setQueryResults }) => {
   return (
     <>
       <div className={styles.editorSection}>
-      <div className={styles.editorToolbar}>
-      <div className={styles.editorHead}>
-        <h4>Input</h4>
-      </div>
-      <div className={styles.editorActions}>
-        <button
-          onClick={handleRunQuery}
-          title="Run Query"
-          className={styles.runButton}
-        >
-          <Play /> Run Query
-        </button>
-        <button title="Save Query">
-          <Save />
-        </button>
-        <button title="Copy Query">
-          <Copy />
-        </button>
-      </div>
-      </div>
+        <div className={styles.editorToolbar}>
+          <div className={styles.editorHead}>
+            <h4>Input</h4>
+          </div>
+          <div className={styles.editorActions}>
+            <button
+              onClick={handleRunQuery}
+              title="Run Query"
+              className={styles.runButton}
+            >
+              <Play /> Run Query
+            </button>
+            <button title="Save Query">
+              <Save />
+            </button>
+            <button title="Copy Query">
+              <Copy />
+            </button>
+          </div>
+        </div>
         <Editor
+          height="100%"
+          defaultLanguage="sql"
           value={currentQuery}
-          onValueChange={(code) => setCurrentQuery(code)}
-          highlight={(code) => highlight(code, languages.sql)}
-          padding={10}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
+          onChange={(code) => setCurrentQuery(code)}
+          options={{
             fontSize: 14,
-            backgroundColor: "#ffffff",
-            color: "#000000",
-            height: "100%",
-            overflow: "auto",
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
           }}
         />
       </div>
