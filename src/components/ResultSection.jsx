81e15002d70843c31c1d4ef5eb10a2298ac2e43c
Ayush Tripathi <ayushflows@gmail.com>
2025-03-26 21:59:03 +0530

    fixed output layout

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -4,33 +4,36 @@ import styles from '../styles/SqlEditor.module.css';
 const ResultSection = ({ queryResults }) => {
   return (
     <div className={styles.resultsSection}>
-    <div className={styles.resultHeader}>
-    <h3>Query Results</h3>
-    </div>
-    <div className={styles.resultsTableContainer}>
-      {queryResults.length > 0 && (
-        <table className={styles.resultsTable}>
-          <thead>
-            <tr>
-              {Object.keys(queryResults[0]).map(key => (
-                <th key={key}>{key}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {queryResults.map((row, index) => (
-              <tr key={index}>
-                {Object.values(row).map((value, i) => (
-                  <td key={i}>{value}</td>
+      <div className={styles.resultHeader}>
+        <h3>Output</h3>
+        <div className={styles.resultActions}>
+
+        </div>
+      </div>
+      <div className={styles.resultsTableContainer}>
+        {queryResults.length > 0 && (
+          <table className={styles.resultsTable}>
+            <thead>
+              <tr>
+                {Object.keys(queryResults[0]).map(key => (
+                  <th key={key}>{key}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody>
+              {queryResults.map((row, index) => (
+                <tr key={index}>
+                  {Object.values(row).map((value, i) => (
+                    <td key={i}>{value}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
-    </div>
-    );
+  );
 };
 
 export default ResultSection;
